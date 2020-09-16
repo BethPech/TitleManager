d5ec7fd7bd79e2a74f7b3ef206792e6957e8e8e2
BethPech <colnze17-94@mail.ru>
2020-09-17 00:54:14 +0300

    Initial commit

---
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -0,0 +1,9 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Movie;
+
+public class MovieManager {
+    private Movie[] items = new Movie[6];
+    items [0] = "Бладшот";
+    {"Бладшот", "Вперёд", "Отель Белград", "Джентельмены", "Человек-невидимка", "Тролли.Мировой тур", "Номер один"};
+}
