d757fa296e1dd1cde1b0a54f37fe28ef1a6a4ed3
BethPech <colnze17-94@mail.ru>
2020-10-20 09:09:13 +0300

    Commit 5

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -6,19 +6,15 @@ public class MovieManager {
     private Movie[] items = new Movie[0];
     int afishaLength = 10;
 
-
-        public void add(Movie item) {
+    public void add(Movie item) {
         int length = items.length + 1;
         Movie[] tmp = new Movie[length];
-        for (int i = 0; i < items.length; i++) {
-            tmp[i] = items[i];
-            System.out.println(items[i]);
-        }
-
+//        for (int i = 0; i < items.length; i++) {
+//            tmp[i] = items[i];
+        System.arraycopy(items, 0, tmp, 0, items.length);
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         items = tmp;
-
     }
 
     public MovieManager[] getAll() {
@@ -30,3 +26,7 @@ public class MovieManager {
         return result;
     }
 }
+
+
+//    Movie movie = new Movie();
+//    Movie movie1 = new Movie(int afishaLength);
--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -1,12 +1,23 @@
 package ru.netology.manager;
 
 import org.junit.jupiter.api.Test;
+import ru.netology.domain.Movie;
 
 public class MovieManagerTest {
 
     @Test
-    public MovieManagerTest() {
+    public void shouldAddMovies() {
+        MovieManager manager = new MovieManager();
+        int idToAdd = 1;
         MovieManager.add("8", "8", "Довод", "боевик");
 
+        MovieManager[] actual = manager.getAll();
+        MovieManager[] expected = new MovieManager[]{"Довод","Номер один","Тролли.Мировой тур", "Человек-невидимка", "Джентльмены", "Отель Белград", "Вперёд","Бладшот"};
+
+
+//    assertEquals(expected, actual);
+        assertArrayEquals(expected, actual);
+
     }
+
 }
